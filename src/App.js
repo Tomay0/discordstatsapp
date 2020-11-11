bdedafb0316008eae9ea940beb54d14146e1f67c
Tomay0 <thomaspage33@gmail.com>
2020-11-11 20:08:13 +1300

    Added test API call

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-  
+
 import logo from './logo.svg';
 import './App.css';
 import Amplify, { API } from 'aws-amplify';
 
-Amplify.configure({API: {
-  endpoints: [
+Amplify.configure({
+  API: {
+    endpoints: [
       {
-          name: "DiscordStatsAPI",
-          endpoint: "https://ai7pkjomr4.execute-api.ap-southeast-2.amazonaws.com/dev"
+        name: "DiscordStatsAPI",
+        endpoint: "https://ai7pkjomr4.execute-api.ap-southeast-2.amazonaws.com/dev"
       }
-  ]
-}});
-
-
-
+    ]
+  }
+});
 
 function App() {
+  const apiName = 'DiscordStatsAPI';
+  const path = '/';
+  const myInit = { // OPTIONAL
+    headers: {}, // OPTIONAL
+    response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+    queryStringParameters: {  // OPTIONAL
+      "type": "num_messages",
+      "time_interval": "alltime",
+      "guild": 746609150842372138n,
+      "timezone": "Pacific/Auckland"
+    },
+  };
+  API
+    .get(apiName, path, myInit)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error.response);
+    });
+
   return (
     <div className="App">
       <header className="App-header">
